f4eab63cf6cb9f135196b221e98a7e71f1d8dc1f
HoLLy <dario.db@hotmail.com>
2017-12-29 03:19:21 +0100

    Get string decrypter type

--- EazFixer/Processors/StringFixer.cs
+++ EazFixer/Processors/StringFixer.cs
@@ -1,23 +1,48 @@
 ﻿using System;
+using System.Linq;
 using dnlib.DotNet;
+using dnlib.DotNet.Emit;
 
 namespace EazFixer.Processors
 {
     internal class StringFixer : IProcessor
     {
+        private MethodDef _decrypterMethod;
+
         public void PreProcess(ModuleDef m)
         {
-            throw new NotImplementedException();
+            //find method
+            _decrypterMethod = Utils.GetMethodsRecursive(m).Single(CanBeStringMethod);
         }
 
         public void Process(ModuleDef m)
         {
+            //load it normally and get all strings by invoking
             throw new NotImplementedException();
         }
 
         public void PostProcess(ModuleDef m)
         {
+            //replace everything
             throw new NotImplementedException();
+        }
+
+        private static bool CanBeStringMethod(MethodDef method)
+        {
+            if (!method.IsStatic || !method.IsAssembly)
+                return false;
+
+            if (method.MethodSig.ToString() != "System.String (System.Int32)")
+                return false;
+
+            if (!method.HasBody || !method.Body.HasInstructions)
+                return false;
+
+            if (!method.Body.Instructions.Any(a => a.OpCode.Code == Code.Call && a.Operand is MethodDef m
+                                                  && m.MethodSig.ToString() == "System.String (System.Int32,System.Boolean)"))
+                return false;
+
+            return true;
         }
     }
 }
---
+++ EazFixer/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using dnlib.DotNet;
+
+namespace EazFixer
+{
+    internal static class Utils
+    {
+        public static IEnumerable<MethodDef> GetMethodsRecursive(ModuleDef t) => t.Types.SelectMany(GetMethodsRecursive);
+        public static IEnumerable<MethodDef> GetMethodsRecursive(TypeDef type)
+        {
+            //return all methods in this type
+            foreach (MethodDef m in type.Methods)
+                yield return m;
+
+            //go through nested types
+            foreach (TypeDef t in type.NestedTypes)
+            foreach (MethodDef m in GetMethodsRecursive(t))
+                yield return m;
+        }
+    }
+}
